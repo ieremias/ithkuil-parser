cd99f952336f0cd5817f8072b465487092325168
Jeremy Morris <morrisjmilton@gmail.com>
2014-12-31 19:47:28 -0600

    Added basic test suite
    Right now it only tests if the words parse, and doesn't test the
    accuracy of the parsing. This may change eventually.

---
+++ TestSuite.hs
@@ -0,0 +1,34 @@
+module Test where
+import Ithkuil
+
+data Test = Test String Bool
+
+passed :: Test -> Bool
+passed (Test _ x) = x 
+result :: Test -> String
+result (Test r _) = r
+
+testAll ts = foldl test1 ([], 0, 0) ts where
+    test1 (results, pass, fail) t
+      | passed t = ((result t) : results, pass + 1, fail)
+      | otherwise = ((result t) : results, pass, fail + 1) 
+   
+main = do mapM_ putStrLn results
+          putStrLn ("Passed: " ++ (show pass))
+          putStrLn ("Failed: " ++ (show fail))
+        where 
+            (results, pass, fail) = testAll tests
+            tests = testParse `map` 
+                [
+                  "phal"
+                , "eqoec"
+                , "uaklarsla"
+                , "uiphawatluxen"
+                , "hremsoqaitsurkoi"
+                , "qhul-lyai'sviksei'arpipto'ks"
+                ]
+                
+testParse x
+  | Right result <- pw x = Test ("PASSED:" ++ x ++ show result) True
+  | Left  result <- pw x = Test ("FAILED:" ++ x ++ show result) False
+
